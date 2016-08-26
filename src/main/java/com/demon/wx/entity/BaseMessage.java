cb810d2a27b4168f003794f551136a37f6bb4490
zDemon <1139912260@qq.com>
2016-08-26 18:15:43 +0800

    add xstream

--- src/main/java/com/demon/wx/entity/BaseMessage.java
+++ src/main/java/com/demon/wx/entity/BaseMessage.java
@@ -1,9 +1,43 @@
 package com.demon.wx.entity;
 
 public class BaseMessage {
+	
+	/** 接收方帐号（收到的OpenID） */
+	protected String toUserName;
+	
+	/** 开发者微信号 */
+	protected String fromUserName;
+	
+	/** 消息创建时间 （整型） */
+	protected Long createTime;
+	
+	/** 消息类型  com.demon.wx.common.MsgType */
+	protected String msgType;
 
-	protected String msgType;	// 消息类型 required
+	public String getToUserName() {
+		return toUserName;
+	}
 
+	public void setToUserName(String toUserName) {
+		this.toUserName = toUserName;
+	}
+
+	public String getFromUserName() {
+		return fromUserName;
+	}
+
+	public void setFromUserName(String fromUserName) {
+		this.fromUserName = fromUserName;
+	}
+
+	public Long getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+	
 	public String getMsgType() {
 		return msgType;
 	}
---
+++ src/main/java/com/demon/wx/entity/Text.java
@@ -0,0 +1,30 @@
+package com.demon.wx.entity;
+
+import com.demon.wx.common.MsgType;
+
+/**
+ * <p>Title: Text</p>
+ * <p>Description: 文本消息</p>
+ * @author dmeng
+ * @date 2016年8月26日 下午6:03:34
+ */
+public class Text extends BaseMessage {
+	
+	/** 文本消息内容 */
+	private String content;
+
+	
+	public Text() {
+		super();
+		this.msgType = MsgType.TEXT.toString().toLowerCase();
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
